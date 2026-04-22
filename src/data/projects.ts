a8114e42fe6e214f90f8d71f60c4000555625304
Linus <elistewesterlinus@gmail.com>
2026-04-22 15:42:05 +0200

    Improve the structure by adding a data folder where most of the information is, making the index much more readable

---
+++ src/data/projects.ts
@@ -0,0 +1,71 @@
+export const projects = [
+  {
+    title: "Delikat flora",
+    description: "A modern website with backend integration and database features.",
+    href: "https://delikat.ntig.dev/products_english.html",
+    image: "/images/delikat-flora.png",
+    imageAlt: "Delikat flora project screenshot",
+    tall: true,
+    langs: [
+      { href: "https://developer.mozilla.org/en-US/docs/Web/HTML", logo: "/images/html-logo.svg", name: "HTML" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/CSS", logo: "/images/css-logo.svg", name: "CSS" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", logo: "/images/js-logo.svg", name: "JavaScript" },
+      { href: "https://nodejs.org/en", logo: "/images/node-js-logo.svg", name: "Node.js" },
+      { href: "https://expressjs.com/", logo: "/images/express-js-logo.svg", name: "Express.js" },
+      { href: "https://supabase.com/", logo: "/images/supabase-logo.svg", name: "Supabase" },
+      { href: "https://www.python.org/", logo: "/images/python-logo.svg", name: "Python" },
+      { href: "https://www.selenium.dev/", logo: "/images/selenium-logo.svg", name: "Selenium" },
+      { href: "https://github.com/NTIG-Uppsala/Delikat-Flora", logo: "/images/github-logo.svg", name: "GitHub" },
+    ]
+  },
+  {
+    title: "Fjellporten",
+    description: "An interactive web application with dynamic content and backend support.",
+    href: "https://fjellporten.ntig.dev/",
+    image: "/images/fjellporten.png",
+    imageAlt: "Fjellporten project screenshot",
+    tall: true,
+    langs: [
+      { href: "https://developer.mozilla.org/en-US/docs/Web/HTML", logo: "/images/html-logo.svg", name: "HTML" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/CSS", logo: "/images/css-logo.svg", name: "CSS" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", logo: "/images/js-logo.svg", name: "JavaScript" },
+      { href: "https://nodejs.org/en", logo: "/images/node-js-logo.svg", name: "Node.js" },
+      { href: "https://expressjs.com/", logo: "/images/express-js-logo.svg", name: "Express.js" },
+      { href: "https://supabase.com/", logo: "/images/supabase-logo.svg", name: "Supabase" },
+      { href: "https://www.python.org/", logo: "/images/python-logo.svg", name: "Python" },
+      { href: "https://www.selenium.dev/", logo: "/images/selenium-logo.svg", name: "Selenium" },
+      { href: "https://github.com/NTIG-Uppsala/Fjellporten", logo: "/images/github-logo.svg", name: "GitHub" },
+    ]
+  },
+  {
+    title: "POS Frontend",
+    description: "WPF based Point of Sale system. With receipt creation and sqlite database integration.",
+    href: "https://github.com/NTIG-Uppsala/TE4POS",
+    image: "/images/POS.png",
+    imageAlt: "TE4POS project screenshot",
+    tall: false,
+    langs: [
+      { href: "https://dotnet.microsoft.com/en-us/languages/csharp", logo: "/images/csharp-logo.svg", name: "C#" },
+      { href: "https://sqlite.org/", logo: "/images/sqlite-logo.svg", name: "SQLite" },
+      { href: "https://github.com/NTIG-Uppsala/TE4POS", logo: "/images/github-logo.svg", name: "GitHub" },
+    ]
+  },
+  {
+    title: "POS Backend",
+    description: "ASP.NET backend for a POS system with APIs and admin panel.",
+    href: "https://github.com/NTIG-Uppsala/TE4POS-Backend",
+    image: "/images/POS-backend.png",
+    imageAlt: "TE4POS Backend project screenshot",
+    tall: false,
+    langs: [
+      { href: "https://developer.mozilla.org/en-US/docs/Web/HTML", logo: "/images/html-logo.svg", name: "HTML" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/CSS", logo: "/images/css-logo.svg", name: "CSS" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", logo: "/images/js-logo.svg", name: "JavaScript" },
+      { href: "https://dotnet.microsoft.com/en-us/apps/aspnet", logo: "/images/aspnet-logo.svg", name: "ASP.NET" },
+      { href: "https://dotnet.microsoft.com/en-us/languages/csharp", logo: "/images/csharp-logo.svg", name: "C#" },
+      { href: "https://sqlite.org/", logo: "/images/sqlite-logo.svg", name: "SQLite" },
+      { href: "https://www.debian.org/", logo: "/images/debian-logo.svg", name: "Debian" },
+      { href: "https://github.com/NTIG-Uppsala/TE4POS-Backend", logo: "/images/github-logo.svg", name: "GitHub" },
+    ]
+  },
+];
---
+++ src/data/skills.ts
@@ -0,0 +1,77 @@
+export const skillCategories = [
+  {
+    title: "Programming Languages",
+    skills: [
+      { href: "https://developer.mozilla.org/en-US/docs/Web/HTML", image: "/images/html-logo.svg", label: "HTML" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/CSS", image: "/images/css-logo.svg", label: "CSS" },
+      { href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", image: "/images/js-logo.svg", label: "JavaScript" },
+      { href: "https://www.typescriptlang.org/", image: "/images/typescript-logo.svg", label: "TypeScript" },
+      { href: "https://www.python.org/", image: "/images/python-logo.svg", label: "Python" },
+      { href: "https://dotnet.microsoft.com/en-us/languages/csharp", image: "/images/csharp-logo.svg", label: "C#" },
+    ]
+  },
+  {
+    title: "Certifications",
+    skills: [
+      { href: "https://www.credly.com/badges/7a4b74fc-9218-408c-927f-d113a38746ec/public_url", image: "/images/html-logo.svg", label: "HTML" },
+      { href: "https://www.credly.com/badges/7a4b74fc-9218-408c-927f-d113a38746ec/public_url", image: "/images/css-logo.svg", label: "CSS" },
+      { href: "https://www.credly.com/badges/0b601a49-fd97-4107-a5a7-c7461e3b43b1/public_url", image: "/images/js-logo.svg", label: "JavaScript" },
+      { href: "https://www.credly.com/badges/02f28b04-4350-4bba-b23d-52db006bdb67/public_url", image: "/images/it-support-logo.svg", label: "IT Support" },
+    ]
+  },
+  {
+    title: "Frameworks",
+    skills: [
+      { href: "https://playwright.dev/", image: "/images/playwright-logo.svg", label: "Playwright" },
+      { href: "https://react.dev/", image: "/images/react-logo.svg", label: "React" },
+      { href: "https://www.selenium.dev/", image: "/images/selenium-logo.svg", label: "Selenium" },
+      { href: "https://www.scrum.org/resources/what-scrum-module", image: "/images/scrum-logo.svg", label: "Scrum" },
+    ]
+  },
+  {
+    title: "Databases",
+    skills: [
+      { href: "https://www.mysql.com/", image: "/images/mysql-logo.svg", label: "MySQL" },
+      { href: "https://sqlite.org/", image: "/images/sqlite-logo.svg", label: "SQLite" },
+      { href: "https://supabase.com/", image: "/images/supabase-logo.svg", label: "Supabase" },
+    ]
+  },
+  {
+    title: "Backend",
+    skills: [
+      { href: "https://nodejs.org/en", image: "/images/node-js-logo.svg", label: "Node.js" },
+      { href: "https://expressjs.com/", image: "/images/express-js-logo.svg", label: "Express.js" },
+      { href: "https://dotnet.microsoft.com/en-us/apps/aspnet", image: "/images/aspnet-logo.svg", label: "ASP.NET" },
+    ]
+  },
+  {
+    title: "OS",
+    skills: [
+      { href: "https://www.microsoft.com/en-us/windows", image: "/images/blue-squares.svg", label: "Windows" },
+      { href: "https://www.raspberrypi.com/", image: "/images/raspberry-pi-logo.svg", label: "Raspbian" },
+      { href: "https://www.debian.org/", image: "/images/debian-logo.svg", label: "Debian" },
+    ]
+  },
+  {
+    title: "Game Engines",
+    skills: [
+      { href: "https://www.pygame.org/news", image: "/images/pygame_logo.svg", label: "Pygame" },
+      { href: "https://www.unity.com/", image: "/images/unity-logo.svg", label: "Unity" },
+      { href: "https://godotengine.org/", image: "/images/godot-logo.svg", label: "Godot" },
+    ]
+  },
+  {
+    title: "Tools",
+    skills: [
+      { href: "https://git-scm.com/", image: "/images/git-logo.svg", label: "Git" },
+      { href: "https://docs.cloud.google.com/iam/docs/service-account-overview", image: "/images/gsa-logo.svg", label: "Service Accounts" },
+    ]
+  },
+  {
+    title: "Professional Skills",
+    skills: [
+      { href: null, image: "/images/team-logo.svg", label: "Teamwork" },
+      { href: null, image: "/images/problem-solving-logo.svg", label: "Problem Solving" },
+    ]
+  },
+];
